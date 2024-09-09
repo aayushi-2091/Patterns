5b1400926cea49ab74717250bde236e90c699def
Aayushi Ghosh <aayushighosh2092001@gmail.com>
2024-09-09 12:31:20 +0530

    p1

---
+++ square1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class square1{
+    public static void main(String args[]){
+        int n=4;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(1);
+            }
+            System.out.println();
+        }
+    }
+}
