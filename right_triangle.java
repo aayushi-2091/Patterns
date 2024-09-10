0f0791a68f043db89b9cb0a79160572927b2e437
Aayushi Ghosh <aayushighosh2092001@gmail.com>
2024-09-10 13:32:57 +0530

    p4

---
+++ right_triangle.java
@@ -0,0 +1,12 @@
+public class right_triangle {
+    public static void main(String[] args) {
+        int n=4,k=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
