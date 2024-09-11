868ce9cff72bf5718c7f9c68a8fcd916ee7197e0
Aayushi Ghosh <aayushighosh2092001@gmail.com>
2024-09-11 12:33:35 +0530

    p8

---
+++ pattern8.java
@@ -0,0 +1,12 @@
+public class pattern8 {
+    public static void main(String[] args) {
+        int n=4,k=3;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(k+" ");
+            }
+            k++;
+            System.out.println();
+        }
+    }
+}
